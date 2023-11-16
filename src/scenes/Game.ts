850128c0fa92fb361a7b0167bad1162d75755a1f
Chanporo <youngchan0518@gmail.com>
2023-11-15 23:19:08 -0800

    Update on playability, some bugs still remain.

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,7 +1,10 @@
 import Phaser, { GameObjects } from 'phaser';
 
 export default class Demo extends Phaser.Scene {
-  LastTime: number;
+  LastTime: number = 0;
+  targetX: number = -1;
+  mouseInput: boolean = false;
+  currentBall: any = null;
 
   constructor() {
     super('GameScene');
@@ -14,7 +17,7 @@ export default class Demo extends Phaser.Scene {
 
   update(time:number, delta:number){
     
-    while(time/1000 > this.LastTime){
+    if(time/1000 > this.LastTime){
 
       const bodies = this.matter.intersectRay(0,50,300,50);
   
@@ -23,18 +26,71 @@ export default class Demo extends Phaser.Scene {
       }
   
       this.LastTime += 1;
-      // this.CreateNewBall();
+    }
+    const targetX = this.getTargetX();
+
+    if(this.mouseInput){
+      const pointer = this.input.activePointer;
+      this.currentBall.setX(pointer.worldX);
+    }
+
+    if(targetX >= 0){
+      const dir = (targetX-150<0)?-1:1; 
+      this.currentBall.setVelocity(dir*3,0);
+      if(Math.abs(targetX-this.currentBall.x)<3){
+        this.setTargetX(-1);
+        this.currentBall.setVelocity(0);
+        this.currentBall.setIgnoreGravity(false);
+      }
     }
   }
 
+
+  create() {
+    //Replace this with matter container for future purpose
+    this.matter.world.setBounds(0,0,300,600);
+    
+    this.currentBall = this.CreateNewBall(150,100,0);
+    //Intersection
+    const line = new Phaser.Geom.Line(0,50,300,50);
+    const graphics = this.add.graphics({lineStyle: {width:1, color: 0xffffff}});
+
+    graphics.strokeLineShape(line);
+
+    this.matter.world.on('sleepstart', (event: any, body: any)=>{
+      event.source.gameObject.setAwake();
+    });
+    this.matter.enableCollisionEventsPlugin();
+    this.input.on('pointerup', (pointer:any) =>
+    {
+      this.mouseInput = false;
+      //TODO: Replace this to relative position
+      if(pointer.worldX<300 && this.currentBall.y == 100 ){
+        if(this.currentBall==null) return;
+        this.setTargetX(pointer.worldX);
+      }
+    });
+    this.input.on('pointerdown', (pointer:any) =>
+    {
+      this.mouseInput = true;
+    });
+
+
+
+  }
+  
   CreateNewBall(x = 150, y = 100, s = 0){
     const newball = this.matter.add.image(x, y, 'ball');
-
+    var zeroCheck = (s==0);
     newball.setDataEnabled();
-    if(s == 0) s = (Math.floor(Math.random()*3)+1);
+
+    //for new balls
+    if(zeroCheck){
+      s = (Math.floor(Math.random()*3)+1);
+    }
 
     newball.data.set('scale',s);
-
+    
     newball.setCircle(400);
     newball.setScale(0.02*s);
     newball.setMass(1*s);
@@ -43,9 +99,15 @@ export default class Demo extends Phaser.Scene {
     newball.setFrictionStatic(0.001);
     newball.setSleepEvents(true,true);
 
-    newball.setOnCollide(pair =>{
+    newball.setOnCollide((pair:any) =>{
       const bodyA = pair.bodyA.gameObject;
       const bodyB = pair.bodyB.gameObject;
+
+      if(this.currentBall==bodyA||this.currentBall==bodyB){
+        this.currentBall.setIgnoreGravity(false);
+        this.targetX = -1;
+        this.currentBall = this.CreateNewBall(150,100,0);
+      }
 
       if(!bodyA||!bodyB){
         return;
@@ -59,36 +121,21 @@ export default class Demo extends Phaser.Scene {
         const midX = (bodyA.x + bodyB.x)/2;
         const midY = (bodyA.y + bodyB.y)/2;
 
-        this.CreateNewBall(midX,midY,scaleA+1);
+        const temp = this.CreateNewBall(midX,midY,scaleA+1);
 
         bodyA.destroy();
         bodyB.destroy();
       }
     });
+    if(zeroCheck) newball.setIgnoreGravity(true);
     return newball;
   }
 
+  setTargetX(x=150){
+    this.targetX = x;
+  }
 
-  create() {
-    //Replace this with matter container for future purpose
-    this.matter.world.setBounds(0,0,300,600);
-    
-    //Intersection
-    const line = new Phaser.Geom.Line(0,50,300,50);
-    const graphics = this.add.graphics({lineStyle: {width:1, color: 0xffffff}});
-
-    graphics.strokeLineShape(line);
-
-    this.matter.world.on('sleepstart', (event, body)=>{
-      event.source.gameObject.setAwake();
-    });
-    this.matter.enableCollisionEventsPlugin();
-    this.input.on('pointerup', pointer =>
-    {
-      this.CreateNewBall(pointer.worldX,100,0);
-    });
-
-
-
+  getTargetX(){
+    return this.targetX;
   }
 }
